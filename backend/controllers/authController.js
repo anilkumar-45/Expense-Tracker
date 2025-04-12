01e24a22288e0b7387b5926cb571a83293bf17b8
anilkumar <anilkumarchary22@gmail.com>
2025-04-12 19:19:49 +0530

    Add backend login, getUserInfo apis

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,7 +43,45 @@ exports.registerUser = async (req, res) => {
 };
 
 // Login User
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error logining user", error: err.message });
+  }
+};
 
 // Get User Info
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error fetching user data", error: err.message });
+  }
+};
+
+
+// 1:13:34 time stamp
